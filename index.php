fd11d2168ac93f34c8f6a632a2a62aaf0b2cbb9d
Matthew Sanders <mms578@g.harvard.edu>
2016-09-13 22:56:44 -0400

    Initial layout

--- index.php
+++ index.php
@@ -5,12 +5,28 @@
 	<title>Matthew Sanders - Project One</title>
 	<meta charset='utf-8'>
 
+    <link href='css/main.css' rel='stylesheet'>
+
 </head>
 <body>
 
-	<?php
-	echo "Project one to come!";
-	?>
+<div id ="AboveBackPhoto">
+    <h1 id="SampleText">Matthew Sanders</h1>
+    <img src="images/msanders.jpg" id="MainImage">
+
+    <h4>Project 2 Title</h2>
+    <p>View Project: [link]</p>
+    <p>View GitHub Repository: [link]</p>
+
+    <h4>Project 3 Title</h2>
+    <p>View Project: [link]</p>
+    <p>View GitHub Repository: [link]</p>
+
+    <h4>Project 3 Title</h2>
+    <p>View Project: [link]</p>
+    <p>View GitHub Repository: [link]</p>
+
+</div>
 
 </body>
 </html>
