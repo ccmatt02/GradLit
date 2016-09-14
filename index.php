5f8dc26af06a79acffee6ab8cfcdc01c9d9aff89
Matthew Sanders <mms578@g.harvard.edu>
2016-09-14 13:02:10 -0400

    index.php and main.css formating changes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>Matthew Sanders - Project One</title>
 	<meta charset='utf-8'>
 
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
     <link href='css/main.css' rel='stylesheet'>
 
 </head>
@@ -12,26 +13,34 @@
 
 <div id ="AboveBackPhoto">
 
-
-    <br>
-
+  
     <h1 id="SampleText">Matthew Sanders</h1>
-    <img src="images/msanders.jpg" id="MainImage">
+  
+  <div class="pure-g">
+    <div class="pure-u-2-5">
 
+  
+      <img src="images/msanders.jpg" id="MainImage">
 
-    <h4>Project 2 Title</h2>
-    <p>View Project: [link]</p>
-    <p>View GitHub Repository: [link]</p>
+    </div>
 
-    <h4>Project 3 Title</h2>
-    <p>View Project: [link]</p>
-    <p>View GitHub Repository: [link]</p>
+    <div class="pure-u-3-5">
 
-    <h4>Project 3 Title</h2>
-    <p>View Project: [link]</p>
-    <p>View GitHub Repository: [link]</p>
+      <h4>Project 2 Title</h4>
+      <p>View Project: [link]</p>
+      <p>View GitHub Repository: [link]</p>
 
-    <br>
+      <h4>Project 3 Title</h4>
+      <p>View Project: [link]</p>
+      <p>View GitHub Repository: [link]</p>
+
+      <h4>Project 3 Title</h4>
+      <p>View Project: [link]</p>
+      <p>View GitHub Repository: [link]</p>
+
+    </div>
+  </div>
+     
 
 </div>
 
