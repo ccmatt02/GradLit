9fd63ce3d13bdd4f5710748520450eb9c6a63aed
Matthew Sanders <Matthew Sanders>
2017-03-26 22:27:38 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Gradually lit</title>
+	<meta charset='utf-8'>
+
+  	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <link href='css/main.css' rel='stylesheet'>
+
+</head>
+<body>
+
+
+
+
+</body>
+</html>
